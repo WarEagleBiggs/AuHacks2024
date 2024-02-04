c8adbb78cd4718325db3f5373eae686d0c6a983c
Adam Biggs <wareaglebiggs@gmail.com>
2024-02-04 03:02:53 -0600

    Lots of little changes

--- Assets/Scripts/CheckBounds.cs
+++ Assets/Scripts/CheckBounds.cs
@@ -2,15 +2,47 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CheckBounds : MonoBehaviour
 {
+    public int crystalCount;
+    public TextMeshProUGUI crystalTxt;
+
+    public int alienCount;
+    public TextMeshProUGUI alienTxt;
+
+    public TextMeshProUGUI objectiveTxt;
+
+    public RotateGuide rGuide;
+    public Transform Ambulance;
+
+    private void Update()
+    {
+        crystalTxt.SetText(crystalCount.ToString() + "/30");
+        alienTxt.SetText(alienCount.ToString() + "/5");
+    }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Bounds")
         {
             Debug.Log("OUT");
             this.transform.position = new Vector3(0, 0, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Crystal")
+        {
+            crystalCount++;
+            Destroy(other.gameObject);
+        } else if (other.tag == "Alien")
+        {
+            //pick up
+            objectiveTxt.SetText("[ ] Find Help");
+            rGuide.playerTransform = Ambulance;
         }
     }
 }
